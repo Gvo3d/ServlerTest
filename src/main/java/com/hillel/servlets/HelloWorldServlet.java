cbfd7cef9520c915c739992387edde22eee37979
Gvo3d <denis.yakimov89@gmail.com>
2016-03-25 10:03:49 +0200

    Servlet test

---
+++ src/main/java/com/hillel/servlets/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package com.hillel.servlets;
+/**
+ * Created by Gvozd on 23.03.2016.
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<p>Hello World</p>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
